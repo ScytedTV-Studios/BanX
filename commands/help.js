03a756a9dfedafa274f120bdd2ab30aee76ad657
Loyd Osborne <loyd.osborne@scyted.tv>
2025-02-08 19:37:10 -0700

    Automatic Bot Update

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,13 @@ const commandsList = require("../commands.json");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("help")
-        .setDescription("Shows a list of commands and their descriptions."),
+        .setDescription("Shows a list of commands."),
     
     async execute(interaction) {
         const embed = new EmbedBuilder()
             .setTitle("Help Menu")
             .setDescription("**Ban X** will automatically delete messages that contain URLs known to link to **X** (formerly **Twitter**). This may additionally support URLs that **share the same values** as the **X** platform.")
-            .setColor(green);
+            .setColor("#ff5050");
 
         for (const cmd of commandsList) {
             embed.addFields({ name: `/${cmd.name}`, value: cmd.description, inline: false });
