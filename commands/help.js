9c2faa4c74b9aece3697ceeedc53a9256ac1d6aa
Loyd Osborne <loyd.osborne@scyted.tv>
2025-02-08 20:38:02 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,16 +7,16 @@ module.exports = {
         .setDescription("Shows a list of commands."),
     
     async execute(interaction) {
+        await interaction.deferReply();
         const embed = new EmbedBuilder()
-            .setTitle("Help | Ban X")
             .setThumbnail("https://github.com/ScytedTV-Studios/BanX/blob/master/Branding/Ban%20X_Twitter%20-%20Logo.jpg?raw=true")
-            .setDescription("**Ban X** will automatically delete messages that contain URLs known to link to **X** (formerly **Twitter**). This may additionally support URLs that **share the same values** as the **X** platform.")
+            .setDescription("**Ban X** will automatically delete messages that contain URLs known to link to **X** (formerly **Twitter**). This may additionally support URLs that **share the same values** as the **X** platform.\n\nStay up-to-date with **Ban X** updates on [Bluesky](https://go.scyted.tv/bsky/banx)!\n\n[ [GitHub](https://github.com/ScytedTV-Studios/BanX) | [Invite Bot](https://discord.com/oauth2/authorize?client_id=1333586687724355759) ]")
             .setColor("#ff5050");
 
         for (const cmd of commandsList) {
             embed.addFields({ name: `${cmd.name}`, value: cmd.description, inline: false });
         }
 
-        await interaction.reply({ embeds: [embed] });
+        await interaction.editReply({ embeds: [embed] });
     }
 };
