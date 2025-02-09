c628b89bfbc99bf0d8d3df7cd968cfcb05f7f02c
Loyd Osborne <loyd.osborne@scyted.tv>
2025-02-09 05:14:19 -0700

    Automatic Bot Update

--- commands/domain.js
+++ commands/domain.js
@@ -58,6 +58,7 @@ module.exports = {
                     embeds: [
                         new EmbedBuilder()
                             .setColor("Green")
+                            .setTitle("<:checkmark:1330976666016550932> `Banned Domains")
                             .setDescription(`\`${domains}\``)
                     ]
                 });
