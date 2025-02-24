d6a0a03c35b1ad28a4caeca7a906f40dd933c94b
Loyd Osborne <loyd.osborne@scyted.tv>
2025-02-24 12:51:39 -0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,8 +9,25 @@ module.exports = {
         await interaction.deferReply();
         const embed = new EmbedBuilder()
         .setColor("Green")
-        .setDescription(`<:checkmark:1330976666016550932> [Invite **Ban X**](https://discord.com/oauth2/authorize?client_id=1333586687724355759)`)
+        .setDescription(`<:checkmark:1330976666016550932> Invite **Ban X**`);
 
-        await interaction.editReply({ embeds: [embed] });
+        const serverButton = new ButtonBuilder()
+            .setLabel("Invite")
+            .setStyle(ButtonStyle.Link)
+            .setURL("https://discord.com/oauth2/authorize?client_id=1333586687724355759")
+
+        const actionRow = new ActionRowBuilder().addComponents(serverButton);
+
+        try {
+            await interaction.editReply({ embeds: [embed], components: [actionRow] });
+        } catch (e) {
+            console.error(`Could not send invite:`, e);
+
+            const embed = new EmbedBuilder()
+                .setColor("Red")
+                .setDescription(`<:crossmark:1330976664535961753> \`Error sending invite: ${e}\``)
+
+            await interaction.editReply({ embeds: [embed] });
+        }
     }
 };
