c45a17a1de6f6c7b6e6f29d783f7446df9d1a942
Loyd Osborne <loyd.osborne@scyted.tv>
2025-02-24 21:31:44 -0700

    Automatic Update

--- bot.js
+++ bot.js
@@ -250,8 +250,8 @@ client.on("messageCreate", async (message) => {
 
     let matchedDomain = null;
 
-    const globalDomainsTrie = categoryTries.get("global") || new Trie();
-    matchedDomain = globalDomainsTrie.containsBannedDomain(message.content);
+    // const globalDomainsTrie = categoryTries.get("global") || new Trie();
+    // matchedDomain = globalDomainsTrie.containsBannedDomain(message.content);
 
     if (!matchedDomain) {
         for (const [category, enabled] of Object.entries(settings)) {
