9dccd7f3fd9bb735183476175056c465823a82d4
Loyd Osborne <loyd.osborne@scyted.tv>
2025-03-10 17:55:18 -0600

    v1.2.2

--- commands/custom.js
+++ commands/custom.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, InteractionContextType } = require("discord.js");
 const axios = require("axios");
 
 const SCYTEDTV_API = process.env.SCYTEDTV_API;
